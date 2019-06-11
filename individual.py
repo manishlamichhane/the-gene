897c3fbec8a1779e11f9b7beaa5808cc3f185006
kausha1n <kaushal.niraula@gmail.com>
2019-06-10 23:45:44 -0400

    Implemented rudimentary inbreeding-prevention logic (#8)
    * Implemented rudimentary inbreeding-prevention logic
    
    - Refactored initial population creation logic to account for parents and children
    - Moved assigning of gender to Male or Female to respective constructor methods
    - Implemented logic to check for in-breeding. Current logic checks for half/full siblings and direct descendants. Have to implement logic for 'Nth cousin kth removed'(see https://www.metaspoon.com/wp-content/uploads/2016/12/familychart.gif ). Caveats: due to increased restriction to breed, male population tends to end up in single digit number often times. Further optimization needed
    
    * [fix] inbreeding-prevention
    
    - the logic checks for family member to permit/disallow procreation based on distance between two parties(DFS)

--- individual.py
+++ individual.py
@@ -1,19 +1,34 @@
+import uuid
+
 import defaults
 import random
 
 from choices import Gender
 from collections import namedtuple
 
-
 class Individual:
-    def __init__(self, **kwargs):
+    def __str__(self):
+        return self.name if self.name else str(self.id)
+
+    def __init__(self, parents=(), **kwargs):
+        # for logging and debugging
+        self.id = uuid.uuid4()
         self.age = 0
         self.gender = kwargs.get('gender') or random.choice(Gender.CHOICES)
         self.ego = kwargs.get('ego') or random.choice(range(0, 100, 5))
         self.health = kwargs.get('health') or random.choice(range(20, 100, 5))
         self.happiness = kwargs.get('happiness') or random.choice(range(0, 100, 5))
         self.intelligence = kwargs.get('intelligence') or random.choice(range(0, 100, 5))
-    
+        self.parents = parents
+        self.name = kwargs.get('name') or ''
+        if parents and len(parents) == 2:
+            # Index 0 is for father
+            parents[0].children.append(self)
+
+            # Index 1 is for mother
+            parents[1].children.append(self)
+        self.children = []
+
     def grow_old(self):
         self.age += 1
         self.intelligence += defaults.Cost.INTELLIGENCE
@@ -48,7 +63,7 @@ class Female(Individual):
         self.strength = kwargs.get('strength') or random.choice(range(20, 50, 5))
         self.is_pregnent = False
         self.feminity = random.choice(range(0, 100, 5))  # NOTE(Manish): determins sex of off-spring
-        super().__init__(**kwargs)
+        super().__init__(gender=Gender.FEMALE, **kwargs)
 
 
 class Male(Individual):
@@ -56,4 +71,4 @@ class Male(Individual):
         self.is_fertile = kwargs.pop('is_fertile ') if 'is_fertile' in kwargs.keys() else random.choice((True, False))
         self.strength = kwargs.get('strength') or random.choice(range(50, 100, 5))
         self.masculinity = random.choice(range(0, 100, 5)) # NOTE(Manish): determins sex of off-spring
-        super().__init__(**kwargs)
+        super().__init__(gender=Gender.MALE, **kwargs)
